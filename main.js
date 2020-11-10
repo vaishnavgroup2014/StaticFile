6f1c3568a86c6d8909353abe507d866b67e57a47
vaishnavgroup2014 <74108751+vaishnavgroup2014@users.noreply.github.com>
2020-11-09 23:25:53 -0800

    initialcommit

---
+++ main.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    $(document).ready(function () {
+        // $.get('/WeatherForecast', function (data) {
+            $.get('https://travelcompany.azurewebsites.net/api/TravelBook/airport', function (data) {
+            //https://travelcompany.azurewebsites.net/api/TravelBook/airport
+            console.log(data);
+        });
+    });
+})();
